69270bd79e7f33d54e99294511c30c821ba12734
Timothy Stewart <timothystewart6@hotmail.com>
2016-05-22 23:01:46 -0500

    initial commit

---
+++ utf-bom.js
@@ -0,0 +1,9 @@
+var bom = require('gulp-bom');
+var convertEncoding = require('gulp-convert-encoding');
+
+gulp.task('utf-bom',function() {
+  gulp.src(['./platforms/windows/www/**/*.js', './platforms/windows/www/**/*.css', './platforms/windows/www/**/*.html'])
+    .pipe(convertEncoding({to: 'utf8'}))
+    .pipe(bom())
+    .pipe(gulp.dest('./platforms/windows/www'));
+});
